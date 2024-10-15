6e2270e7d63fe59bf2abcac33f06b4fd1241bf0b
mujahid-khan111 <khanmujahid7003@gmail.com>
2024-10-16 02:23:48 +0530

    My-login-page

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const container = document.querySelector("")
+const numberOFSquares = 800;
+
+for (let i = o; i < numberOFSquares; i++) {
+    const square = document.createElement("datalist")
+    square.classList.add("square");
+    container.appendChild(square);
+}
